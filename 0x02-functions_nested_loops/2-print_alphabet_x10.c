790784fcc7a0a6bd6bff4d3bb85881a70b6c67df
Hatem-rk <hatem.rekhaies@gmail.com>
2022-03-15 16:34:26 -0700

    2- 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+print_alphabet_x10 ()
+{
+int i=0;
+char alpha="a";
+while (i<10)
+{
+while (alpha<z)
+{
+_putchar(alpha);
+alpha++;
+}
+i++;
+}
+
+}
