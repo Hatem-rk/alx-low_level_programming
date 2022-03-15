0cb77b41ed48d6870dd253a73465de9b7e9b7944
Hatem-rk <hatem.rekhaies@gmail.com>
2022-03-15 15:21:14 -0700

    fix some issues

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+   Char sh = "_putchar";
+  while(sh){
+    _putchar(sh);
+    sh++;
+   
+  } 
+  _putchar('\n');
 
-	return (0);
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,8 @@
-ain.h:14:1: error: stray ‘\302’ in program
-   14 | © 2022 GitHub, Inc.
-      | ^
-main.h:14:2: error: stray ‘\251’ in program
-   14 | © 2022 GitHub, Inc.
-      |  ^
-main.h:14:4: error: expected identifier or ‘(’ before numeric constant
-   14 | © 2022 GitHub, Inc.
-      |    ^~~~
-In file included from main.c:1:
-main.h:14:1: error: stray ‘\302’ in program
-   14 | © 2022 GitHub, Inc.
-      | ^
-main.h:14:2: error: stray ‘\251’ in program
-   14 | © 2022 GitHub, Inc.
-      |  ^
-main.h:14:4: error: expected identifier or ‘(’ before numeric constant
-   14 | © 2022 GitHub, Inc.     |    ^~~~
+#include <unistd.h>
+   
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
+
 ~
