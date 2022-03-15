00062bdaad750621a67d7a9ad2893b35f42e305f
Hatem-rk <hatem.rekhaies@gmail.com>
2022-03-15 10:04:32 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,19 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-© 2022 GitHub, Inc.
-Terms
+ain.h:14:1: error: stray ‘\302’ in program
+   14 | © 2022 GitHub, Inc.
+      | ^
+main.h:14:2: error: stray ‘\251’ in program
+   14 | © 2022 GitHub, Inc.
+      |  ^
+main.h:14:4: error: expected identifier or ‘(’ before numeric constant
+   14 | © 2022 GitHub, Inc.
+      |    ^~~~
+In file included from main.c:1:
+main.h:14:1: error: stray ‘\302’ in program
+   14 | © 2022 GitHub, Inc.
+      | ^
+main.h:14:2: error: stray ‘\251’ in program
+   14 | © 2022 GitHub, Inc.
+      |  ^
+main.h:14:4: error: expected identifier or ‘(’ before numeric constant
+   14 | © 2022 GitHub, Inc.     |    ^~~~
+~
